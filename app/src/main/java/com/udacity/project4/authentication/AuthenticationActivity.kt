2b9a74969c9d8b1723f4a00bfa620e78c75bc1ee
dev7odaa <mah.khalil22@gmail.com>
2022-09-10 23:54:32 +0200

    "build launchSignInFlow method to make user login with E-mail & Google Account"

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -3,6 +3,7 @@ package com.udacity.project4.authentication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityAuthenticationBinding
 
@@ -11,6 +12,11 @@ import com.udacity.project4.databinding.ActivityAuthenticationBinding
  * signed in users to the RemindersActivity.
  */
 class AuthenticationActivity : AppCompatActivity() {
+
+    companion object {
+        const val SIGN_IN_RESULT_CODE = 1001
+        const val TAG = "MainFragment"
+    }
 
     private lateinit var binding : ActivityAuthenticationBinding
 
@@ -21,4 +27,19 @@ class AuthenticationActivity : AppCompatActivity() {
         binding.authButton.setOnClickListener { launchSignInFlow() }
 
     }
+
+    private fun launchSignInFlow() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).build(), Companion.SIGN_IN_RESULT_CODE
+        )
+    }
+
+
 }
